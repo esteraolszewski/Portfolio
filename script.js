05a219bed4210e4c78c95f4fdaf27efaa209bf96
esteraolszewski <estera.o@live.ca>
2023-01-25 16:53:43 -0500

    Time to upgrade! Adding JS and new styling :nail_care:

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Hamburger menu help from tutorial on Youtube by Web Dev Tutorials
+// Hambrger Menu
+    // Hamburger menu help from tutorial on Youtube by Web Dev Tutorials
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
@@ -12,3 +13,24 @@ document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", (
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+// Header Image Loop
+    // Help from tutorial on Youtube by Adam Khoury
+
+
+let image = document.getElementById('image');
+let images = ['headerphoto1.png', 'headerphoto2.png', 'headerphoto3.png', 'headerphoto4.png'];
+let totalImages = images.length;
+let i = 0;
+
+function loop() {
+    if(i > (totalImages - 1)) {
+        i = 0;
+    }
+    image.innerHTML = '<img src="styles/Images/'+images[i]+'">';
+    i++;
+    loopTimer = setTimeout('loop()', 1000);
+}
+
+loop();
+
