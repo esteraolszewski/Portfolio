6a60f0a853ec6260fa69c7411af1f013c21f33dc
esteraolszewski <estera.o@live.ca>
2023-01-26 17:04:07 -0500

    created back to top button

--- script.js
+++ script.js
@@ -34,3 +34,21 @@ function loop() {
 
 loop();
 
+// Back to Top Button
+
+let myButton = document.getElementById('topButton');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+};
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
